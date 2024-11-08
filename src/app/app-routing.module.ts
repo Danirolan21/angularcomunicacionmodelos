68f37ad5f3dd40ec0207a59931b2222d226383e1
Danirolan21 <danirolan21@hotmail.com>
2024-11-08 10:34:10 +0100

    Padre E Hijo Coches

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaproductosComponent } from './components/listaproductos/listaproductos.component';
+import { HomeComponent } from './components/home/home.component';
+import { PadrecochesComponent } from './components/padrecoches/padrecoches.component';
 
 const routes: Routes = [
-  {path: "listaproductos", component: ListaproductosComponent}
+  {path: "", component: HomeComponent},
+  {path: "listaproductos", component: ListaproductosComponent},
+  {path: "padrecoches", component: PadrecochesComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaproductosComponent } from './components/listaproductos/listaproductos.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { HijococheComponent } from './components/hijocoche/hijocoche.component';
+import { HomeComponent } from './components/home/home.component';
+import { PadrecochesComponent } from './components/padrecoches/padrecoches.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaproductosComponent,
-    MenuComponent
+    MenuComponent,
+    HijococheComponent,
+    HomeComponent,
+    PadrecochesComponent,
   ],
   imports: [
     BrowserModule,
